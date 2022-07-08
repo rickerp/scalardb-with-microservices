2ba9365b36a45854a7eb2fc121982ca06d06768a
Ricardo Fernandes <pdgrickerp@gmail.com>
2022-07-08 11:12:29 +0900

    Initial commit with Java dependecies

---
+++ src/main/java/jp/keio/acds/microscalar/MicroscalarApplication.java
@@ -0,0 +1,13 @@
+package jp.keio.acds.microscalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroscalarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroscalarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/keio/acds/microscalar/MicroscalarApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.keio.acds.microscalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroscalarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
